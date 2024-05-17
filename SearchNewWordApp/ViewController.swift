f1bd33e18a36ec1fa79b4018e47f42e09549e40c
yeggrrr <yjkim981103@gmail.com>
2024-05-17 19:24:30 +0900

    [Feat] 코드에 outlet 연결

--- SearchNewWordApp/ViewController.swift
+++ SearchNewWordApp/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchNewWordViewController: UIViewController {
 
+    @IBOutlet var textFieldView: UIView!
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var searchButton: UIButton!
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBOutlet var newWordButton1: UIButton!
+    @IBOutlet var newWordButton2: UIButton!
+    @IBOutlet var newWordButton3: UIButton!
+    @IBOutlet var newWordButton5: UIButton!
+    @IBOutlet var newWordButton4: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    
+    
+    
 
 }
 
