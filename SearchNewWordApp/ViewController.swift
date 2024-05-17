0b548717f4c1de26107808a4bf24d13b6bd5b1f6
yeggrrr <yjkim981103@gmail.com>
2024-05-17 18:27:11 +0900

    Initial Commit

---
+++ SearchNewWordApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchNewWordApp
+//
+//  Created by YJ on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
