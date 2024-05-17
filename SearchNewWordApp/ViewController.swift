ca77fe5fe8f32b89b45213e36c29cb56d767582f
yeggrrr <yjkim981103@gmail.com>
2024-05-17 19:39:31 +0900

    [Feat] configureUI 추가

--- SearchNewWordApp/ViewController.swift
+++ SearchNewWordApp/ViewController.swift
@@ -13,7 +13,6 @@ class SearchNewWordViewController: UIViewController {
     @IBOutlet var searchTextField: UITextField!
     @IBOutlet var searchButton: UIButton!
     
-    @IBOutlet var backgroundImageView: UIImageView!
     @IBOutlet var descriptionLabel: UILabel!
     
     @IBOutlet var newWordButton1: UIButton!
@@ -24,8 +23,67 @@ class SearchNewWordViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
     }
 
+    func configureUI() {
+        
+        // textFieldView
+        textFieldView.backgroundColor = .black
+        
+        // searchTextField
+        searchTextField.placeholder = "신조어를 입력해주세요."
+        searchTextField.tintColor = .black
+        searchTextField.keyboardType = .default
+        searchTextField.returnKeyType = .default
+        
+        // searchButton
+        searchButton.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButton.setTitle("검색", for: .normal)
+        searchButton.tintColor = .white
+        
+        // descriptionLabel
+        descriptionLabel.textColor = .black
+        descriptionLabel.font = .boldSystemFont(ofSize: 18)
+        descriptionLabel.textAlignment = .center
+        
+        // newWordButton1
+        newWordButton1.setTitle("1", for: .normal)
+        newWordButton1.tintColor = .black
+        newWordButton1.titleLabel?.font = .systemFont(ofSize: 11)
+        newWordButton1.backgroundColor = .lightGray
+        newWordButton1.layer.cornerRadius = 5
+        
+        // newWordButton2
+        newWordButton2.setTitle("2", for: .normal)
+        newWordButton2.tintColor = .black
+        newWordButton2.titleLabel?.font = .systemFont(ofSize: 11)
+        newWordButton2.backgroundColor = .lightGray
+        newWordButton2.layer.cornerRadius = 5
+        
+        // newWordButton3
+        newWordButton3.setTitle("3", for: .normal)
+        newWordButton3.tintColor = .black
+        newWordButton3.titleLabel?.font = .systemFont(ofSize: 11)
+        newWordButton3.backgroundColor = .lightGray
+        newWordButton3.layer.cornerRadius = 5
+        
+        // newWordButton4
+        newWordButton4.setTitle("4", for: .normal)
+        newWordButton4.tintColor = .black
+        newWordButton4.titleLabel?.font = .systemFont(ofSize: 11)
+        newWordButton4.backgroundColor = .lightGray
+        newWordButton4.layer.cornerRadius = 5
+        
+        // newWordButton5
+        newWordButton5.setTitle("5", for: .normal)
+        newWordButton5.tintColor = .black
+        newWordButton5.titleLabel?.font = .systemFont(ofSize: 12)
+        newWordButton5.backgroundColor = .lightGray
+        newWordButton5.layer.cornerRadius = 5
+    }
+    
     
     
     
